770edceb69ec4454fc67892732d9cf55683b5432
Muhammad Ardhie <m.ardhie5325@gmail.com>
2024-11-09 11:48:20 +0700

    UI Version 1: Bottom Navigatio & Chatbot XML

---
+++ app/src/main/java/com/example/nourishpath/ui/ChatbotActivity.kt
@@ -0,0 +1,19 @@
+package com.example.nourishpath.ui
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.nourishpath.R
+import com.example.nourishpath.databinding.ActivityChatbotBinding
+
+class ChatbotActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityChatbotBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityChatbotBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
