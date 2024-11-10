a00ddd20f1e2a9974dfb0e54e0b4856a6cbc6f55
Muhammad Ardhie <m.ardhie5325@gmail.com>
2024-11-10 17:44:35 +0700

    Profile-UI Version 1

--- app/src/main/java/com/example/nourishpath/ui/guides/GuidesFragment.kt
+++ app/src/main/java/com/example/nourishpath/ui/guides/GuidesFragment.kt
@@ -1,4 +1,4 @@
-package com.example.nourishpath.ui.notifications
+package com.example.nourishpath.ui.guides
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -7,11 +7,12 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import com.example.nourishpath.databinding.FragmentNotificationsBinding
+import com.example.nourishpath.databinding.FragmentDashboardBinding
 
-class NotificationsFragment : Fragment() {
+class GuidesFragment : Fragment() {
 
-    private var _binding: FragmentNotificationsBinding? = null
+    private var _binding: FragmentDashboardBinding? = null
+
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -22,14 +23,14 @@ class NotificationsFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val notificationsViewModel =
-            ViewModelProvider(this).get(NotificationsViewModel::class.java)
+        val guidesViewModel =
+            ViewModelProvider(this).get(GuidesViewModel::class.java)
 
-        _binding = FragmentNotificationsBinding.inflate(inflater, container, false)
+        _binding = FragmentDashboardBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textNotifications
-        notificationsViewModel.text.observe(viewLifecycleOwner) {
+        val textView: TextView = binding.textDashboard
+        guidesViewModel.text.observe(viewLifecycleOwner) {
             textView.text = it
         }
         return root
@@ -39,4 +40,5 @@ class NotificationsFragment : Fragment() {
         super.onDestroyView()
         _binding = null
     }
+
 }
