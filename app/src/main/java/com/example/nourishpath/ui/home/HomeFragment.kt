fa7e7d1605f654a2639c3fe07691a62ce77bc94f
peen3104 <116818986+peen3104@users.noreply.github.com>
2024-11-11 21:39:38 +0700

    Update settings dark mode, tanggal

--- app/src/main/java/com/example/nourishpath/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/nourishpath/ui/home/HomeFragment.kt
@@ -1,23 +1,26 @@
 package com.example.nourishpath.ui.home
 
+import android.content.Context
 import android.content.Intent
+import android.content.SharedPreferences
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.viewModels
-import androidx.lifecycle.ViewModelProvider
 import com.example.nourishpath.databinding.FragmentHomeBinding
 import com.example.nourishpath.ui.profile.ProfileActivity
+import java.text.SimpleDateFormat
+import java.util.*
 
 class HomeFragment : Fragment() {
 
     private var _binding: FragmentHomeBinding? = null
     private val binding get() = _binding!!
 
-    private val homeViewModel by viewModels<HomeViewModel>()
+    private lateinit var sharedPreferences: SharedPreferences
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -29,11 +32,39 @@ class HomeFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        // Menampilkan tanggal hari ini
+        val dateFormat = SimpleDateFormat("dd MMMM yyyy", Locale.getDefault())
+        val todayDate = dateFormat.format(Date())
+        binding.tvTodayDate.text = todayDate
+
+        sharedPreferences = requireContext().getSharedPreferences("user_preferences", Context.MODE_PRIVATE)
+
+        val isDarkMode = sharedPreferences.getBoolean("DARK_MODE", false)
+        binding.themeSwitch.isChecked = isDarkMode
+        setDarkMode(isDarkMode)
+
         binding.ivProfilePicture.setOnClickListener {
             val intent = Intent(requireContext(), ProfileActivity::class.java)
             startActivity(intent)
         }
+        binding.themeSwitch.setOnCheckedChangeListener { _, isChecked ->
+            setDarkMode(isChecked)
+            with(sharedPreferences.edit()) {
+                putBoolean("DARK_MODE", isChecked)
+                apply()
+            }
+        }
     }
+
+    private fun setDarkMode(isDarkMode: Boolean) {
+        if (isDarkMode) {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+        } else {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+        }
+    }
+
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
